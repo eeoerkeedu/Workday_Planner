5a221778b19a44d952c98f8f21be0192d3d26909
LilTzar <eeoerkeedu@gmail.com>
2022-02-17 21:57:52 -0700

    established the initialize funtion and store funtion

--- script.js
+++ script.js
@@ -8,64 +8,77 @@ var workHours = [
   {
     time: "8:00 am",
     hour: 08,
-    appt: "",
+    event: "",
   },
   {
     time: "9:00 am",
     hour: 09,
-    appt: "",
+    event: "",
   },
   {
     time: "10:00 am",
     hour: 10,
-    appt: "",
+    event: "",
   },
   {
     time: "11:00 am",
     hour: 11,
-    appt: "",
+    event: "",
   },
   {
     time: "12:00 pm",
     hour: 12,
-    appt: "",
+    event: "",
   },
   {
     time: "1:00 pm",
     hour: 13,
-    appt: "",
+    event: "",
   },
   {
     time: "2:00 pm",
     hour: 14,
-    appt: "",
+    event: "",
   },
   {
     time: "3:00 pm",
     hour: 15,
-    appt: "",
+    event: "",
   },
   {
     time: "4:00 pm",
     hour: 16,
-    appt: "",
+    event: "",
   },
   {
     time: "5:00 pm",
     hour: 17,
-    appt: "",
+    event: "",
   },
   {
     time: "6:00 pm",
     hour: 18,
-    appt: "",
+    event: "",
   },
 ];
 
-// function storeAppts ( {
+// puts object into local storage
+function storeEvents() {
+  localStorage.setItem("workHours", JSON.stringify(workHours));
+}
 
-// })
+function init() {
+  var savedEvent = JSON.parse(localStorage.getItem(workHours));
 
+  if (savedEvent !== null) {
+    workHours = savedEvent;
+  }
+
+  storeEvents();
+  displayEvents();
+}
+
+// Formats body of page with time blocks to write in
 workHours.forEach(function (assignAppend) {
   var hourBlockEl = $("<form>");
   var saveButton = $("<button>");
@@ -75,24 +88,32 @@ workHours.forEach(function (assignAppend) {
   var saveIcon = $('<i class="far fa-save fa-lg"></i>');
   var hourBoxHour = assignAppend.time;
 
+  // creates time block rows with add-ons
   hourBlockEl.addClass("row justify-content-center time-block");
   hourBlockEl.append(hourBox);
   hourBlockEl.append(eventBox);
   hourBlockEl.append(saveButton);
 
+  //sets hour div's and their formatting
   hourBox.addClass("col-1 d-flex align-items-center hour");
   hourBox.text(hourBoxHour);
 
+  //formats main time blocks and adds text field to write in events
   eventBox.addClass("col-7 future description");
   eventInput.attr("id", assignAppend.hour);
   eventBox.append(eventInput);
 
+  //builds save icon
   saveButton.append(saveIcon);
   saveButton.addClass("col-1  saveBtn");
 
+  //runs function to check time
   eventTimeSet();
+
+  //adds made rows to their parent
   timeBlocksEL.append(hourBlockEl);
 
+  //checks formatting for the fields to color code
   function eventTimeSet() {
     var HH = moment().get("hour");
 
@@ -107,16 +128,6 @@ workHours.forEach(function (assignAppend) {
     }
   }
 });
-
-// function handApptSubmit(event) {
-//   event.preventDefault();
-
-//   const appt = document.querySelector("input").val;
-
-//   console.log(appt);
-// }
-
-// saveBtnEl.on("click", handApptSubmit);
 
 // displays today's date
 $("#currentDay").text(today.format("MMM Do, YY"));
