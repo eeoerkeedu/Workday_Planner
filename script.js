1f9857f5191e3df653e8be7407be05927aa362ca
LilTzar <eeoerkeedu@gmail.com>
2022-02-17 19:32:03 -0700

    added save icons to buttons

--- script.js
+++ script.js
@@ -9,31 +9,10 @@ $("#currentDay").text(today.format("MMM Do, YY"));
 
 function init() {
   for (var i = 0; i < workHours.length; i++) {
-    var hourBlockEl = $("<form>");
-    var saveButton = $("<button>");
-    var hourBox = $("<div>");
     var eventBox = $("<input>");
-    // var saveIcon = $('<FontAwesomeIcon icon="fa-regular fa-floppy-disk" />');
     var hourBoxHour = workHours[i];
 
     eventTimeSet();
-
-    //   hourBlockEl.addClass(
-    //     "row justify-content-center vw-100 time-block hourRow"
-    //   );
-    //   hourBlockEl.append(hourBox);
-    //   hourBlockEl.append(eventBox);
-    //   hourBlockEl.append(saveButton);
-
-    //   hourBox.addClass("col-1 d-flex align-items-center hour");
-    //   hourBox.text(hourBoxHour + ":00");
-
-    //   eventBox.addClass("col-6 future description text-dark appointmentInput");
-
-    //   // saveButton.append(saveIcon);
-    //   saveButton.addClass("col-1 saveBtn");
-
-    //   timeBlocksEL.append(hourBlockEl);
   }
 
   function eventTimeSet() {
@@ -51,5 +30,15 @@ function init() {
     }
   }
 }
+
+function handApptSubmit(event) {
+  event.preventDefault();
+
+  const appt = document.querySelector("input").val;
+
+  console.log(appt);
+}
+
+saveBtnEl.on("click", handApptSubmit);
 
 init();
