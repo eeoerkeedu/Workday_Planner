1eb6b83ad1cc2ae1d37f976ec4845a552c486f7d
LilTzar <eeoerkeedu@gmail.com>
2022-02-17 20:45:29 -0700

    changed array to array of objects

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
 var timeBlocksEL = $("#timeblocks");
 var saveBtnEl = $(".saveBtn");
 var eventBoxEL = $(".eventBox");
-
-var workHours = [08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-
-// displays today's date
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YY"));
+
+//objects to determine hours blocks and appt info
+var workHours = [
+  {
+    time: "8:00 am",
+    hour: 08,
+    appt: "",    
+  },
+  {
+    time: "9:00 am",
+    hour: 09,
+    appt: "",    
+  },
+  {
+    time: "10:00 am",
+    hour: 10,
+    appt: "",    
+  },
+  {
+    time: "11:00 am",
+    hour: 11,
+    appt: "",    
+  },
+  {
+    time: "12:00 pm",
+    hour: 12,
+    appt: "",    
+  },
+  {
+    time: "1:00 pm",
+    hour: 13,
+    appt: "",    
+  },
+  {
+    time: "2:00 pm",
+    hour: 14,
+    appt: "",    
+  },
+  {
+    time: "3:00 pm",
+    hour: 15,
+    appt: "",    
+  },
+  {
+    time: "4:00 pm",
+    hour: 16,
+    appt: "",    
+  },
+  {
+    time: "5:00 pm",
+    hour: 17,
+    appt: "",    
+  },
+  {
+    time: "6:00 pm",
+    hour: 18,
+    appt: "",    
+  }
+];
+
+
+function storeAppts ( {
+  
+})
 
 function init() {
   for (var i = 0; i < workHours.length; i++) {
@@ -26,7 +85,8 @@ function init() {
     hourBox.addClass("col-1 d-flex align-items-center hour");
     hourBox.text(hourBoxHour + ":00");
 
-    eventBox.addClass("col-9 future description");
+    eventBox.addClass("col-7 future description");
+    eventInput.attr("id", [i]);
     eventBox.append(eventInput);
 
     saveButton.append(saveIcon);
@@ -52,14 +112,17 @@ function init() {
   }
 }
 
-function handApptSubmit(event) {
-  event.preventDefault();
+// function handApptSubmit(event) {
+//   event.preventDefault();
 
-  const appt = document.querySelector("input").val;
+//   const appt = document.querySelector("input").val;
 
-  console.log(appt);
-}
+//   console.log(appt);
+// }
 
-saveBtnEl.on("click", handApptSubmit);
+// saveBtnEl.on("click", handApptSubmit);
 
+// displays today's date
+$("#currentDay").text(today.format("MMM Do, YY"));
+//initializes planner on page load
 init();
